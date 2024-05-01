4af55e6341886543db54f33fc06f91460e4c8fb7
JSamuel <sp4619168@gmail.com>
2024-05-01 11:01:19 -0600

    Crear formulario de inicio de sesión

---
+++ app/Views/auth/login.php
@@ -0,0 +1,26 @@
+<?= $this->extend('layout/base.php') ?>
+
+<?= $this->section('contenido') ?>
+<div class="container">
+    <div class="row align-items-center">
+        <div class="col-12 col-md-6">
+            <img src="images/logo.png" alt="Logo" class="img-fluid">
+        </div>
+        <div class="col-12 col-md-6">
+            <h1 class="mb-5">Inicia sesión</h1>
+            <form method="post" action="login">
+                <div class="mb-3">
+                    <input type="email" class="form-control form-control-lg" id="email" placeholder="Email" name="email">
+                </div>
+                <div class="mb-3">
+                    <input type="password" class="form-control form-control-lg" id="password" placeholder="Contraseña" name="password">
+                </div>
+                <div class="d-grid">
+                    <button type="submit" class="btn btn-primary btn-lg">Iniciar sesión</button>
+                </div>
+                <p class="text-center mt-2">¿Aún no tienes una cuenta? <a href="signup">Registrate</a></p>
+            </form>
+        </div>
+    </div>
+</div>
+<?= $this->endSection() ?>
