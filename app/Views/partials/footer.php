d38dfd753d1da2624a1aee4cfa1c1772d616d0a7
JSamuel <sp4619168@gmail.com>
2024-05-01 11:05:51 -0600

    Diseñar footer

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -1,3 +1,5 @@
-<footer>
-  CopyTickets &copy; 2024 | Todos los derechos reservados
+<footer class="bg-secondary-subtle py-4">
+    <p class="text-center text-body-secondary mb-0">
+        CopyTickets &copy; 2024 | Todos los derechos reservados
+    </p>
 </footer>
