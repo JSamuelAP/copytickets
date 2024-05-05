d6d40d2cd7e987cfc7f48e2d0d90f7942eb17c7b
JSamuel <sp4619168@gmail.com>
2024-05-05 16:07:49 -0600

    Corregir enlaces del navbar

--- app/Views/partials/navbar.php
+++ app/Views/partials/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-        <a class="navbar-brand" href="./">
-            <img src="images/logo.png" alt="Logo" width="48">
+        <a class="navbar-brand" href="<?= base_url('public') ?>">
+            <img src="<?= base_url('public/images/logo.png') ?>" alt="Logo" width="48">
             CopyTickets
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
@@ -11,15 +11,15 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="./">Eventos</a>
+                    <a class="nav-link" href="<?= base_url('public') ?>">Eventos</a>
                 </li>
             </ul>
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a href="login" class="nav-link">Login</a>
+                    <a href="<?= base_url('public/login') ?>" class="nav-link">Login</a>
                 </li>
                 <li class="nav-item">
-                    <a href="signup" class="nav-link">Signup</a>
+                    <a href="<?= base_url('public/signup') ?>" class="nav-link">Signup</a>
                 </li>
             </ul>
         </div>
