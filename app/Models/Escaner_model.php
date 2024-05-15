6ee43ec26bd8dabb095cb5448ad0c9dad58898b4
JSamuel <sp4619168@gmail.com>
2024-05-14 21:39:58 -0600

    Devolver metodos que se borraron

--- app/Models/Escaner_model.php
+++ app/Models/Escaner_model.php
@@ -9,6 +9,22 @@ class Escaner_Model extends Model
   protected $table = 'escaners';
   protected $primaryKey = 'id';
   protected $returnType = 'array';
-  protected $allowedFields = ['id', 'evento_id', "usuario", "password"];
+  protected $allowedFields = ['evento_id', 'usuario', "password"];
   protected bool $allowEmptyInserts = false;
+
+  function login($usuario, $password)
+  {
+    return $this->where('usuario', $usuario)
+      ->where('password', $password)
+      ->first();
+  }
+
+  function findEscanerByID(string $id)
+  {
+    $escaner = $this->where('id', $id)->first();
+
+    if (!$escaner) return throw new \Exception('No se encontró el escaner con tal id');
+
+    return $escaner;
+  }
 }
